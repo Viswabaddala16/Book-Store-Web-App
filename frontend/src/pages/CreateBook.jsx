69c806fd8d868348ee70ad58b6696ce99ae3ed6e
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2024-11-24 21:33:20 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -40,7 +40,7 @@ function CreateBook() {
 
     setLoading(true);
     try {
-      await axios.post('http://localhost:5555/books', data, {
+      await axios.post('https://book-store-backend-lsnz.onrender.com', data, {
         headers: { Authorization: `Bearer ${token}` },
       });
       enqueueSnackbar('Book Created Successfully!', { variant: 'success' });
