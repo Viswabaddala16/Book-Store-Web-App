3fda3ed8ad7d65c5c16ff27200994c1196eafc18
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2025-03-18 10:54:23 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,9 +27,9 @@ function Home() {
       try {
         setLoading(true);
 <<<<<<< HEAD
-        const response = await axios.get('http://localhost:5555/books', {
+        const response = await axios.get('https://book-store-web-app-backend.onrender.com/books', {
 =======
-        const response = await axios.get('https://book-store-web-backend.onrender.com/books', {
+        const response = await axios.get('https://book-store-web-app-backend.onrender.com/books', {
 >>>>>>> 64c8f012440da791c553601e60d1582a4eebd93b
           headers: { Authorization: `Bearer ${token}` },
         });
